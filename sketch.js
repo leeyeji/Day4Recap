1dc401f8649a72b706b08d9bff8a0ae476182146
leeyeji <miracle458393@gmail.com>
2014-09-25 13:41:38 +0900

    add 0925
    1

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup()
+{
+	creatcanvas(800, 600);
+	background(0);
+	noFill();
+	stroke(255, 0, 0);
+	ellipse(400, 300, 100, 100);
+}
+
+function draw()
+{
+	ellipse(400, 300, mouseX, mouseY);
+}
